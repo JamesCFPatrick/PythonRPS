77bf0d763bdfd26a828aa46326dcf79e48a637dc
JamesCFPatrick <88326473+JamesCFPatrick@users.noreply.github.com>
2021-10-05 17:26:00 +1100

    Update Jett Python Help.py

--- Jett Python Help.py
+++ Jett Python Help.py
@@ -8,7 +8,6 @@ import random
 """
 3 double quotations and you can have multiline comments!
 Make sure you remember to close your comment otherwise it will comment all your code out.
-:)
 """
 
 # Defining all main variables needed below.
@@ -20,62 +19,98 @@ loss = str("\nUh oh, you lost! Prepare yourself!\n")
 #Inputs pulled from this list below are case sensitive!
 rpsList = ["Rock", "Paper", "Scissors"]
 
-#Only 5 activity variables for the time being.
+#7 win activities.
 
-act1 = str("\nDo 10 starjumps!")
-act2 = str("\nDo a 30 second plank!")
-act3 = str("\nDo a 180kg deadlift! HA!!")
-act4 = str("\nDo 15 burpees!")
-act5 = str("\nDo 10 pushups!")
+actw1 = str("\nDo 15 star jumps!")
+actw2 = str("\nDo 10 second plank!")
+actw3 = str("\nDo 10 lunges!")
+actw4 = str("\nDo 10 pushups!")
+actw5 = str("\nDo 10 crunches!")
+actw6 = str("\nDo a 15 second wall sit!")
+actw7 = str("\nDo 10 mountain climbers!")
 
-#Activity function is called when the game is lost.
+#7 loss activities.
 
-def actFunction():
-	actList = [act1, act2, act3, act4, act5]
-	actChoose = input("\nPick an activity - 1, 2, 3 or 5: ")
+actl1 = str("\nDo 15 star jumps!")
+actl2 = str("\nDo 10 second plank!")
+actl3 = str("\nDo 10 lunges!")
+actl4 = str("\nDo 15 burpees!")
+actl5 = str("\nDo 10 pushups!")
+actl6 = str("\nDo a thing.")
+actl7 = str("\nDo 10 mountain climbers!")
+
+
+#Activity functions are called depending on win or loss.
+
+def actWin():
+	actWinList = [actw1, actw2, actw3, actw4, actw5]
+	actChoose = input("\nPick an activity - 1, 2, 3, 4, 5, 6 or 7: ")
 	if actChoose == "1":
-		print(actList[0])
+		print(actWinList[0])
 	if actChoose == "2":
-		print(actList[1])
+		print(actWinList[1])
 	if actChoose == "3":
-		print(actList[2])
+		print(actWinList[2])
 	if actChoose == "4":
-		print(actList[3])
+		print(actWinList[3])
 	if actChoose == "5":
-		print(actList[4])
+		print(actWinList[4])
+
+def actLoss():
+	actLossList = [actl1, actl2, actl3, actl4, actl5, actl6, actl7]
+	actChoose = input("\nPick an activity - 1, 2, 3, 4, 5, 6 or 7: ")
+	if actChoose == "1":
+		print(actLossList[0])
+	if actChoose == "2":
+		print(actLossList[1])
+	if actChoose == "3":
+		print(actLossList[2])
+	if actChoose == "4":
+		print(actLossList[3])
+	if actChoose == "5":
+		print(actLossList[4])
+	if actChoose == "5":
+		print(actLossList[5])
+	if actChoose == "6":
+		print(actLossList[6])
 
 #This will be the main loop of the game below.
 
 while True:
-	yourChoice = input("\nRock, Paper or Scissors?: ")
+	#yourChoice = input("\nRock, Paper or Scissors?: ")
+	try:
+		yourChoice = str(input("\nRock, Paper or Scissors?: "))
+	except ValueError:
+		print("Invalid input.")
+		continue
+	else:
+		break
 	rpsComp = random.choice(rpsList)
 	print(f"\nYou chose {yourChoice}, the computer chose {rpsComp}.\n")
-
-
 	if yourChoice == rpsComp:
 		print(tie)
 		continue
 	elif yourChoice == "Rock":
 		if rpsComp == "Scissors":
 			print(win)
-			continue
+			actWin()
 		else: 
 			print(loss)
-			actFunction()
+			actLoss()
 	elif yourChoice == "Paper":
 		if rpsComp == "Rock":
 			print(win)
-			continue
+			actWin()
 		else:
 			print(loss)
-			actFunction()
+			actLoss()
 	elif yourChoice == "Scissors":
 		if rpsComp == "Paper":
 			print(win)
-			continue
+			actWin()			
 		else:
 			print(loss)
-			actFunction()
+			actLoss()
 
 	playAgain = input("\nDo you want to play again? Y/N: ")				
 	#This if statement checks if N is entered. Then it prints a goodbye message and uses the break statement to end the loop.
